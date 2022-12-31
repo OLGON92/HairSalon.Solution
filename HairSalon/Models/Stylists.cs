2020f4d1bce63ed6a86b3e0652eadd048ec35ac9
OLGON92 <ogon318@gmail.com>
2022-12-31 15:30:03 -0800

    add logic

---
+++ HairSalon/Models/Stylists.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public string Name { get; set; }
+    public string Details { get; set; }
+    public int CategoryId { get; set;}
+    public List<Client> Clients { get; set; }
+  }
+}

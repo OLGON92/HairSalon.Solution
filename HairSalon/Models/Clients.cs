2b2e9daf71cb061deb67982b2ce5545de3a388cc
OLGON92 <ogon318@gmail.com>
2022-12-31 16:06:18 -0800

    add efcore stuff

--- HairSalon/Models/Clients.cs
+++ HairSalon/Models/Clients.cs
@@ -4,7 +4,7 @@ namespace HairSalon.Models
 {
   public class Client
   {
-    public string CliName { get; set; }
+    public string Description { get; set; }
     public int ClientId { get; set; }
     public int StylistId { get; set; }
     public Stylist Stylist { get; set; }

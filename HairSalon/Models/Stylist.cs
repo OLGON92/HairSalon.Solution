2bd0eb2c0ccc290bff12204ae44ec8c9e70033e9
OLGON92 <ogon318@gmail.com>
2022-12-31 17:53:56 -0800

    add anything that was missed previously

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -6,7 +6,7 @@ namespace HairSalon.Models
   {
     public string Name { get; set; }
     public string Details { get; set; }
-    public int CategoryId { get; set;}
+    public int StylistId { get; set;}
     public List<Client> Clients { get; set; }
   }
 }
